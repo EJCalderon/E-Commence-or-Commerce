80dbc6758f036aa052aa09e4fe9a4333e0223875
EJCalderon <emiliojaviercalderon@gmail.com>
2022-02-23 01:33:46 -0600

    removed develop

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,7 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-
-require('dotenv').config();
-
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
